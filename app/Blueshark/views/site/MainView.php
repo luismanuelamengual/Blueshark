fd32460236c375ce2519ffcc2c7bb43d5e442d59
luis <luis@luis-VGN-N330FH>
2013-07-28 12:01:58 -0300

    Cambios de estilo en la pagina HOME

--- app/Blueshark/views/site/MainView.php
+++ app/Blueshark/views/site/MainView.php
@@ -3,7 +3,6 @@
 namespace Blueshark\views\site;
 
 use NeoPHP\web\view\HTMLView;
-use NeoPHP\widget\html\Tag;
 
 abstract class MainView extends HTMLView
 {
@@ -20,14 +19,11 @@ abstract class MainView extends HTMLView
         $this->addMeta(array("http-equiv"=>"Content-Type", "content"=>"text/html; charset=UTF-8"));
         $this->addMeta(array("charset"=>"utf-8"));
         $this->addMeta(array("name"=>"viewport", "content"=>"width=device-width, initial-scale=1.0"));
-        $this->addStyleFile("assets/bootstrap/css/bootstrap.min.css");
+        $this->addStyleFile("assets/bootstrap-3.0.0RC1/css/bootstrap.min.css");
         $this->addStyleFile("assets/font-awesome/css/font-awesome.css");
         $this->addStyleFile("css/style.css");
-        $this->addStyleFile("assets/bootstrap/css/bootstrap-responsive.min.css");    
-        $this->headTag->add('        <!--[if lt IE 9]><script src="js/html5shim.js"></script><![endif]-->');
-        $this->headTag->add('        <link rel="shortcut icon" href="images/favicon.png" />');
-        $this->addScriptFile("js/jquery.min.js");
-        $this->addScriptFile("assets/bootstrap/js/bootstrap.min.js");
+        $this->addScriptFile("http://code.jquery.com/jquery.js");
+        $this->addScriptFile("assets/bootstrap-3.0.0RC1/js/bootstrap.min.js");
     }
     
     protected function buildBody ()
