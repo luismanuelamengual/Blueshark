99c0fd1c1044e19468b843a2b9ef9d2ab668cc25
luis <luis@luis-VGN-N330FH>
2013-08-04 12:08:05 -0300

    Subo cambios de estilo para las vistas de Blueshark

--- app/Blueshark/views/site/HomeView.php
+++ app/Blueshark/views/site/HomeView.php
@@ -7,23 +7,20 @@ class HomeView extends MainView
     protected function buildBody ()
     {
         $this->bodyTag->add($this->createNavbar());
-        $this->bodyTag->add($this->createBody());
-        $this->bodyTag->add($this->createFooter());
     }
     
     protected function createNavBar ()
-    {
-        
+    {   
         return '
-        <div class="navbar navbar-fixed-top navbar-main">
+        <div class="navbar navbar-inverse navbar-fixed-top bs-docs-nav">
             <div class="container">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
+                <a href="../" class="navbar-brand">Blueshark</a>
+                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Blueshark</a>
-                <div class="nav-collapse collapse navbar-responsive-collapse">
+                <div class="nav-collapse collapse bs-navbar-collapse">
                     <ul class="nav navbar-nav pull-right">
                         <li class="dropdown">            
                             <a data-toggle="dropdown" class="dropdown-toggle" href="#"><i class="icon-user"></i> ' . $this->getSession()->firstName . ' ' . $this->getSession()->lastName . ' <b class="caret"></b></a>
@@ -34,113 +31,7 @@ class HomeView extends MainView
                             </ul>
                         </li>
                     </ul>
-                </div>
-            </div>
-        </div>';
-    }
-    
-    protected function createBody ()
-    {
-        return '
-                <div class="content">
-
-                ' . $this->createSideBar() . '
-
-                <!-- Main bar -->
-                <div class="mainbar">
-
-              <!-- Page heading -->
-              <div class="page-head">
-                <h2 class="pull-left"><i class="icon-file-alt"></i> Error Log</h2>
-
-                <!-- Breadcrumb -->
-                <div class="bread-crumb pull-right">
-                  <a href="index.html"><i class="icon-home"></i> Home</a> 
-                  <!-- Divider -->
-                  <span class="divider">/</span> 
-                  <a href="#" class="bread-current">Dashboard</a>
-                </div>
-
-                <div class="clearfix"></div>
-
-              </div>
-              <!-- Page heading ends -->
-
-                    <!-- Matter -->
-
-                    <div class="matter">
-                <div class="container-fluid">
-                  <div class="row-fluid">
-                    <div class="span12">
-
-                      <div class="widget">
-                        <div class="widget-head">
-                          <div class="pull-left">Error Log</div>
-                          <div class="widget-icons pull-right">
-                            <a href="#" class="wminimize"><i class="icon-chevron-up"></i></a> 
-                            <a href="#" class="wclose"><i class="icon-remove"></i></a>
-                          </div>  
-                          <div class="clearfix"></div>
-                        </div>
-                        <div class="widget-content">
-                          <div class="padd">
-
-                            <div class="error-log">
-                              <ul>
-                              </ul>
-                            </div>
-
-                          </div>
-                          <div class="widget-foot">
-                            <!-- Footer goes here -->
-                          </div>
-                        </div>
-                      </div>  
-
-                    </div>
-                  </div>
-                </div>
-                          </div>
-
-                        <!-- Matter ends -->
-
-            </div>
-
-           <!-- Mainbar ends -->	    	
-           <div class="clearfix"></div>
-
-        </div>';
-    }
-    
-    protected function createFooter ()
-    {
-        return '
-        <footer>
-            <div class="container-fluid">
-                <div class="row-fluid">
-                    <div class="span12">
-                        <p class="copy">Copyright &copy; 2012 | <a href="#">Blueshark</a> </p>
-                    </div>
-                </div>
-            </div>
-        </footer>';
-    }
-    
-    protected function createSideBar ()
-    {
-        return '
-        <div class="sidebar">
-            <div class="sidebar-dropdown"><a href="#">Navigation</a></div>
-                <ul id="nav">
-                    <li><a href="index.html"><i class="icon-home"></i> Dashboard</a></li>
-                    <li class="has_sub"><a href="#"><i class="icon-list-alt"></i> Widgets  <span class="pull-right"><i class="icon-chevron-right"></i></span></a>
-                        <ul>
-                            <li><a href="widgets1.html">Widgets #1</a></li>
-                            <li><a href="widgets2.html">Widgets #2</a></li>
-                            <li><a href="widgets3.html">Widgets #3</a></li>
-                        </ul>
-                    </li>  
-                </ul>
+                 </div>
             </div>
         </div>';
     }
